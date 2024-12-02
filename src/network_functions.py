569002c3f23e9a63d1a70eab7fb5689d865a9497
Shane Tucker <shaneatucker@outlook.com>
2024-12-02 15:24:48 -0600

    Added Port Scanner
    Added Port Scan functionality to network_functions.py

--- src/network_functions.py
+++ src/network_functions.py
@@ -1,6 +1,7 @@
 import geocoder
-import socket
+from socket import *
 from scapy.all import *
+
 
 #Finds source location of packet. If it cannot find location (eg. originates on private network), returns None
 def geolocate(ip):
@@ -17,6 +18,29 @@ def det_port_scan():
     pass
     
 #Built in port scanner to find vulnerabilities on network
-def find_open_ports():
-    open_ports = {}
+#scan_port is what actually detects if the port is open or not
+def scan_port(ip, port, open_ports, lock): 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)
+    connection = s.connect_ex((ip, port))
     
+    if connection == 0:
+        with lock: 
+            open_ports.append(port)
+    s.close()
+
+#port_scanner uses multithreading to increase the speed of the scanner
+def port_scanner(ip): 
+    open_ports = []
+    lock = threading.Lock() #Thread lock to ensure only one thread can change open_ports at a time
+    threads = [] #List to keep track of threads
+    
+    for i in range(1, 65536): #Checks every port
+        thread = threading.Thread(target=scan_port, args=(ip, i, open_ports, lock))
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join() #Join all threads to prevent premature ending
+
+    return open_ports
