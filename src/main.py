148ee5a6c163ec09b3da758f1bbb6dbfca523bbc
Bradley-Mitchell-UAH <79287813+Bradley-Mitchell-UAH@users.noreply.github.com>
2024-12-06 13:24:58 -0600

    Test reading specific atributes from captured packets

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ packets = []
 
 # Callback function to process each packet
 def packet_callback(pkt):
+    print('\n', pkt)
+    #ls(pkt)
+    print('source ip: ', pkt[IP].src)
+    print('destination ip: ', pkt[IP].dst)
+    print('source port', pkt[IP].sport)
+    print('destination port', pkt[IP].dport)
+    print('length: ', pkt[IP].len)
     packets.append(pkt)
 
 # Function to start sniffing in the background
@@ -20,17 +27,23 @@ def save_to_database():
         # WIP, will save packet information to DB when done
 
 # Start the sniffing in a separate thread
-sniff_thread = threading.Thread(target=start_sniffing)
-sniff_thread.daemon = True
-sniff_thread.start()
+#sniff_thread = threading.Thread(target=start_sniffing)
+#sniff_thread.daemon = True
+#sniff_thread.start()
 
 # Start the saving function in the background
-save_thread = threading.Thread(target=save_to_database)
-save_thread.daemon = True
-save_thread.start()
+#save_thread = threading.Thread(target=save_to_database)
+#save_thread.daemon = True
+#save_thread.start()
 
-try:
-    while True:
-        pass  # Infinite loop to keep the program running
-except KeyboardInterrupt:
-    print("Sniffing stopped.")
+aysnc_sniff = AsyncSniffer(prn=packet_callback, iface=None, count=5)
+aysnc_sniff.start()
+aysnc_sniff.join()
+print("Sniffing stopped.")
+
+# TODO: replace with a system that can actually be stopped
+#try:
+#    while True:
+#        pass  # Infinite loop to keep the program running
+#except KeyboardInterrupt:
+#    print("Sniffing stopped.")
