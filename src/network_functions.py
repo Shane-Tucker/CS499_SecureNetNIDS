5e7a5ad747482c758393f49e791a25eb546a94f4
Shane Tucker <shaneatucker@outlook.com>
2024-12-06 15:25:52 -0600

    Added port scan detection
    Added a new function called all_detections to network_functions.py to create a single function call that can be used to start all detection threads without having to call each independently from main. Also added port scan detection that tests to see if a host attempts to connect to multiple different ports on a single other host.

--- src/network_functions.py
+++ src/network_functions.py
@@ -1,7 +1,22 @@
 import geocoder
 from socket import *
 from scapy.all import *
+from queue import Queue
 
+def all_detection(packets): 
+    alerts = Queue()
+    
+    threads = [] #Keep track of threads
+    #Start port scan detection
+    det_port_scan_thread = threading.Thread(target=det_port_scan, args=(packets, alerts,))
+    det_port_scan_thread.daemon = True
+    threads.append(det_port_scan_thread)
+    det_port_scan_thread.start()
+    
+    for thread in threads: 
+        thread.join()
+        
+    return alerts
 
 #Finds source location of packet. If it cannot find location (eg. originates on private network), returns None
 def geolocate(ip):
@@ -13,10 +28,30 @@ def geolocate(ip):
     
 #Detects if a port scan occurs
 #Works by testing if a computer attempts to connect to multiple different ports on a different computer within a short timespan
-#May create false positives if host is running program that purposefully uses multiple ports
-def det_port_scan(): 
-    pass
-    
+#While this does catch large scale port scans, it cannot detect targeted port scans that attempt to see if just one port is open
+#Additionally, it is prone to false positives if a host requires a connection to multiple ports
+def det_port_scan(packets, alerts): 
+    counts = {} #Dict to keep track of all src to dst packets
+    min_att_ports = 10 #Sets a minimum of the amount of ports that an host needs to try to connect to to be considered a scan
+                      #If set to 10, for example, if host A tries to attempt to 10+ ports on host B, considered an attempted scan
+    while not packets.empty(): 
+        p = packets.get()
+        if p.haslayer(IP) and p.haslayer(TCP):  # Ensure packet has IP and TCP layers
+            full_ip = f"{p[IP].src}_{p[IP].dst}"
+            if full_ip in counts: #Add to unique ports for ip pair
+                if p.dport not in counts[full_ip]: 
+                    counts[full_ip].append(p.dport)
+            else: 
+                counts.update({full_ip:[p.dport]})
+                    
+    for i in counts: 
+        if len(counts[i]) >= min_att_ports: #Compares the list of unique ports to the set minimum
+            divide = i.find("_") #Splits the string into source and dest ip
+            ip_src = i[0:divide]
+            ip_dst = i[divide + 1:]
+            message = "Potential port scan from " + ip_src + " to " + ip_dst #Creates message
+            alerts.put(message)
+            
 #Built in port scanner to find vulnerabilities on network
 #scan_port is what actually detects if the port is open or not
 def scan_port(ip, port, open_ports, lock): 
